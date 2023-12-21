e7bf9a9f5508a53b81e3df0dfe2d90f540cde442
Harshit Shukla <mrharshit2409@gmail.com>
2023-12-21 21:51:15 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,23 +1,53 @@
 const {City} = require('../models/index');
 
 class CityRepository {
-    async createCity({ name }){
+
+    async createCity({ name }) {
         try{
-            const city = await City.create({ name });
+            const city = await City.create({
+                name
+            });
             return city;
-        }catch(error){
-            throw{error};
+        } catch(error) {
+            console.log("something went wrong in the repo layer");
+            throw {error};
         }
     }
 
-    async deleteCity({cityId}){
+    async deleteCity(cityId) {
         try{
             await City.destroy({
                 where: {
                     id: cityId
                 }
             });
-        }catch(error){
+            return true;
+        } catch (error) {
+            console.log("Something went wrong inside the repo layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try{
+            const city = await City.update(data,{
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong inside the repo layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try{
+            const city = await City.findByPk(cityId);
+            return City;
+        } catch (error) {
+            console.log("Something went wrong inside the repo layer");
             throw {error};
         }
     }

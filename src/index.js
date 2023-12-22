002ebfd74172dd7478ef11a55426e2efc2b9fff3
Harshit Shukla <mrharshit2409@gmail.com>
2023-12-22 23:21:51 +0530

    updated city.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const {PORT} = require('./config/serverConfig');
+
+const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
+const db = require('./models/index');
 const setupAndStartServer = async () => {
+
     const app = express();
 
-    app.use(bodyParser.json);
+    app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
     app.use('/api', ApiRoutes);
 
-    app.listen(PORT , async () => {
-        console.log(`server started at ${PORT}`);
+    app.listen(PORT, async () => {
+        console.log(`Server started at ${PORT}`);
+        if(process.env.SYNC_DB) {
+            db.sequelize.sync({alter: true});
+        }
     });
 }
 
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,5 +1,3 @@
-const { Op } = require('sequelize');
-
 const { City } = require('../models/index');
 
 class CityRepository {
@@ -11,13 +9,13 @@ class CityRepository {
             });
             return city;
         } catch (error) {
-            console.log("Something went wrong in the repo layer");
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
 
     async deleteCity(cityId) {
-        try{
+        try {
             await City.destroy({
                 where: {
                     id: cityId
@@ -25,31 +23,38 @@ class CityRepository {
             });
             return true;
         } catch (error) {
-            console.log("Something went wrong inside the repo layer");
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
 
-    async updateCity(cityId, data) {
-        try{
-            const city = await City.update(data,{
-                where: {
-                    id: cityId
-                }
-            });
+    async updateCity(cityId, data) { // {name: "Prayagraj"}
+        try {
+            // The below approach also works but will not return updated object
+            // if we are using Pg then returning: true can be used, else not
+            // const city = await City.update(data, {
+            //     where: {
+            //         id: cityId
+            //     },
+            //      
+            // });
+            // for getting updated data in mysql we use the below approach
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
             return city;
         } catch (error) {
-            console.log("Something went wrong inside the repo layer");
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
 
     async getCity(cityId) {
-        try{
+        try {
             const city = await City.findByPk(cityId);
             return city;
         } catch (error) {
-            console.log("Something went wrong inside the repo layer");
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }

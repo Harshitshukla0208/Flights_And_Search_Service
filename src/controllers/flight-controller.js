a2205aca6453c03ddb479f8eb46562a2f574d7fc
Harshit Shukla <mrharshit2409@gmail.com>
2023-12-25 22:50:23 +0530

    Completed Flight Update API

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,10 +1,11 @@
 const {FlightService} = require('../services/index');
-const {SuccessCodes} = require('../utils/error-codes');
+const { SuccessCodes } = require('../utils/error-codes');
+
 const flightService = new FlightService();
-const create = async(req, res) => {
+
+const create = async (req, res) => {
     try {
-        //below written flightRequest is used to not allow any random data being inserted in the database
-        let flightRequestData = {
+        const flightRequestData = {
             flightNumber: req.body.flightNumber,
             airplaneId: req.body.airplaneId,
             departureAirportId: req.body.departureAirportId,
@@ -32,15 +33,16 @@ const create = async(req, res) => {
 }
 
 const getAll = async (req, res) => {
-    try{
+    try {
+        console.log(req.query);
         const response = await flightService.getAllFlightData(req.query);
         return res.status(SuccessCodes.OK).json({
             data: response,
             success: true,
             err: {},
             message: 'Successfully fetched the flights'
-        })
-    }catch(error) {
+        });
+    } catch (error) {
         console.log(error);
         return res.status(500).json({
             data: {},
@@ -50,7 +52,50 @@ const getAll = async (req, res) => {
         });
     }
 }
+
+const get = async (req, res) => {
+    try {
+        const response = await flightService.getFlight(req.params.id);
+        return res.status(SuccessCodes.OK).json({
+            data: response,
+            success: true,
+            err: {},
+            message: 'Successfully fetched the flight'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to fetch the flight',
+            err: error
+        });
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const response = await flightService.updateFlight(req.params.id, req.body);
+        return res.status(SuccessCodes.OK).json({
+            data: response,
+            success: true,
+            err: {},
+            message: 'Successfully updated the flight'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to update the flight',
+            err: error
+        });
+    }
+}
+
 module.exports = {
     create,
-    getAll
+    getAll,
+    get,
+    update
 }
